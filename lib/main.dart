adb7d2bf111722ada98642a265785ba7d30fbd0b
Chit Ye Aung <chityeaung.tu@gmail.com>
2019-12-06 13:37:37 +0630

    Base Config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cooking_guide/home/home.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -17,22 +20,66 @@ class App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.deepPurpleAccent),
       home: Scaffold(
-        appBar: AppBar(title: Text("Cooking Guide"), actions: <Widget>[IconButton(icon: Icon(Icons.search),onPressed: (){},),IconButton(icon: Icon(Icons.add_to_photos), onPressed: (){},), ],),
-        bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: Colors.transparent,
-          height: 60,
-          color: Colors.deepPurple,
-          onTap: (index)=> print(index),
-          items: <Widget>[
-            Icon(Icons.apps, size: 30, color: Colors.white,),
-            Icon(Icons.location_on, size: 30, color: Colors.white,),
-            Icon(Icons.favorite_border, size: 30,color: Colors.white,),
-            Icon(Icons.list, size: 30, color: Colors.white,),
-            Icon(Icons.person_pin_circle, size: 30, color: Colors.white,),
-          ],
-        ),
-        body: Home()
-      ),
+          appBar: AppBar(
+            title: Text("Cooking Guide"),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.add_to_photos),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          bottomNavigationBar: CurvedNavigationBar(
+            backgroundColor: Colors.transparent,
+            height: 60,
+            color: Colors.deepPurple,
+            onTap: (index) => {
+              // Firestore.instance
+              //     .collection("users")
+              //     .getDocuments()
+              //     .then((snap) => {
+              //      print(snap.documents[1].data)
+              //     })
+              // FacebookLogin().logIn(['email', 'public_profile']).then((onValue){
+              //   AuthCredential credential = FacebookAuthProvider.getCredential(accessToken: onValue.accessToken.token);
+              //   FirebaseAuth.instance.signInWithCredential(credential).then((onValue){
+              //     print(onValue.user.displayName);
+              //   });
+              // })
+            },
+            items: <Widget>[
+              Icon(
+                Icons.apps,
+                size: 30,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.location_on,
+                size: 30,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.favorite_border,
+                size: 30,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.list,
+                size: 30,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.person_pin_circle,
+                size: 30,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          body: Home()),
     );
   }
 }
